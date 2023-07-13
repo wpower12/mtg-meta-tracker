13eb62eb354836d95659ed54da6c5a337e0172fb
wpower12 <willpowe@gmail.com>
2023-07-13 13:23:27 -0400

    fixed created_db issues.

--- create_db.py
+++ create_db.py
@@ -3,24 +3,24 @@ import mysql.connector
 
 DB_NAME = "mtg_meta_tracker"
 
-CREATE_TABLES = """
-    CREATE TABLE `deck` (
+TABLES = [
+    """CREATE TABLE `deck` (
       `iddeck` varchar(32) NOT NULL,
       `desc` mediumtext,
       `color` varchar(5) NOT NULL,
       PRIMARY KEY (`iddeck`)
-    );
-    CREATE TABLE `game` (
+    );""",
+    """CREATE TABLE `game` (
       `idgame` int NOT NULL AUTO_INCREMENT,
       `date` datetime NOT NULL,
       `notes` mediumtext,
       PRIMARY KEY (`idgame`)
-    );
-    CREATE TABLE `player` (
+    );""",
+    """CREATE TABLE `player` (
       `idplayer` varchar(64) NOT NULL,
       PRIMARY KEY (`idplayer`)
-    );
-    CREATE TABLE `games_played` (
+    );""",
+    """    CREATE TABLE `games_played` (
       `idgame` int NOT NULL,
       `idplayer` varchar(64) NOT NULL,
       `iddeck` varchar(32) NOT NULL,
@@ -31,13 +31,18 @@ CREATE_TABLES = """
       CONSTRAINT `fk_games_played_1` FOREIGN KEY (`idgame`) REFERENCES `game` (`idgame`),
       CONSTRAINT `fk_games_played_2` FOREIGN KEY (`idplayer`) REFERENCES `player` (`idplayer`),
       CONSTRAINT `fk_games_played_3` FOREIGN KEY (`iddeck`) REFERENCES `deck` (`iddeck`)
-    );
-"""
+    );""",
+]
 
 cnx = mysql.connector.connect(user=config('DB_USER'), password=config('DB_PASSWORD'), host=config('DB_HOST'))
 cur = cnx.cursor()
 
-cur.execute(f"CREATE DATABSE {DB_NAME};")
+cur.execute(f"CREATE DATABASE {DB_NAME};")
+cnx.commit()
+
 cur.execute(f"USE {DB_NAME};")
-cur.execute(CREATE_TABLES)
+cnx.commit()
+
+for TABLE in TABLES:
+    cur.execute(TABLE)
 cnx.commit()
