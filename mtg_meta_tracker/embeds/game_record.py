d61a6906fcae62f95a8a581fa2a9f526180716cb
wpower12 <willpowe@gmail.com>
2023-08-05 08:42:58 -0400

    Adding game id to game record embed.

--- mtg_meta_tracker/embeds/game_record.py
+++ mtg_meta_tracker/embeds/game_record.py
@@ -4,7 +4,7 @@ PLACE_STRS = ["1st", "2nd", "3rd", "4th"]
 
 class GameRecordEmbed(discord.Embed):
 
-    def __init__(self, player_data, date, notes):
+    def __init__(self, player_data, date, notes, game_id=None):
         super(GameRecordEmbed, self).__init__(title="Game Record")
 
         self.add_field(name=date, value=notes, inline=False)
@@ -12,6 +12,9 @@ class GameRecordEmbed(discord.Embed):
             if pd is not None:
                 p, d = pd
                 self.add_field(name=f"{PLACE_STRS[n]}", value=f"{p} - {d}")
+
+        if game_id is not None:
+            self.add_field(name=f"", value=f"Game ID: {game_id}", inline=False)
 
 
 
